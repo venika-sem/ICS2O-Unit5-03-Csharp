2c6ac7b429d52214792a0bdefa514131ef4842a1
Venika Sem <99354917+venika-sem@users.noreply.github.com>
2022-05-22 17:57:35 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,34 @@
+// Created by: Venika Sem
+// Created on: Apr 2022
+//
+// This program checks if a person is eligible for movie rates.
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int userAge;
+
+        Console.WriteLine("Write your age:");
+        userAge = int.Parse(Console.ReadLine());
+        if (userAge > 17)
+        {
+            Console.WriteLine("You are eligible to watch R rated movies alone.");
+        }
+        else if (userAge > 12)
+        {
+            Console.WriteLine("You are eligible to watch PG 13 rated movies alone.");
+        }
+        else if (userAge > 4)
+        {
+            Console.WriteLine("You are eligible to watch G and PG rated movies alone.");
+        }
+        else
+        {
+            Console.WriteLine("You cannot watch any rated movies alone");
+        }
+        Console.WriteLine("Done.");
+    }
 }
